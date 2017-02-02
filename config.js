ea872c25dee976e7ea8eb4586fd39f238a8f7f33
Pawan <pawan@saket.dk>
2017-02-02 18:39:22 +0530

    again update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://<pawan9124>:<serious777>@jello.modulusmongo.net:27017/a8qeNesa'
+	'database': 'mongodb://pawan9124:serious777@jello.modulusmongo.net:27017/a8qeNesa'
 }
